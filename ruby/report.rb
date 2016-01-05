e77f633d6012508527a3a981dce1691f6991e5e8
kirikiriyamama <kirikiriyamama@icloud.com>
2016-01-06 02:33:21 +0900

    Add another reporter which implemented by Ruby

---
+++ ruby/report.rb
@@ -0,0 +1,39 @@
+require 'bundler'
+Bundler.require
+
+ses = Aws::SES::Client.new
+cw  = Aws::CloudWatch::Client.new
+
+quota = ses.get_send_quota
+
+statistics        = ses.get_send_statistics.send_data_points.sort_by(&:timestamp).last(4)
+delivery_attempts = statistics.map(&:delivery_attempts).inject(&:+)
+bounces           = statistics.map(&:bounces).inject(&:+)
+complaints        = statistics.map(&:complaints).inject(&:+)
+rejects           = statistics.map(&:rejects).inject(&:+)
+
+cw.put_metric_data(
+  namespace: 'SES',
+  metric_data: [
+    {
+      metric_name: 'QuotaUsed',
+      value: quota.sent_last_24_hours / quota.max_24_hour_send * 100,
+      unit: 'Percent'
+    },
+    {
+      metric_name: 'BounceRate',
+      value: bounces.to_f / delivery_attempts * 100,
+      unit: 'Percent'
+    },
+    {
+      metric_name: 'ComplaintRate',
+      value: complaints.to_f / delivery_attempts * 100,
+      unit: 'Percent'
+    },
+    {
+      metric_name: 'RejectRate',
+      value: rejects.to_f / delivery_attempts * 100,
+      unit: 'Percent'
+    }
+  ]
+)
